fdc0437af8ba15d55437922208dfe9be83e58e6f
alaye771 <65874110+alaye771@users.noreply.github.com>
2025-08-17 12:47:43 +0200

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -1,37 +1,33 @@
 class Elements {
 	constructor() {
-		this.puzzle = document.querySelector('.puzzle');
-		this.cellsAmount = 20;
-		this.puzzleDivs = [];
-		this.draggableDivs = [];
-		this.cells = document.querySelector('.cells');
+        this.puzzle = document.querySelector('.puzzle')
+		this.cellsAmount = 20
+		this.puzzleDivs = []
+		this.draggableDivs = [] 
+		this.cells = document.querySelector('.cells')
 		this.modal = document.querySelector('.modal');
-		this.modalText = document.querySelector('.modal-text'); // corrigé
-		this.modalBtn = document.querySelector('.modal-btn');
-		this.attempt = document.querySelector('.attempt');
+        this.modalText = document.querySelector('.modal-text'); 
+        this.modalBtn = document.querySelector('.modal-btn');
+        this.attempt = document.querySelector('.attempt');
 		this.finalImg = document.querySelector('.final-img');
-		this.inputFile = document.getElementById('input-file');
-		this.loader = document.querySelector('.loader');
-		this.randomBtn = document.querySelector('.random-btn'); // corrigé
+        this.inputFile = document.getElementById('input-file'); 
+        this.createElements()
+     }
 
-		this.createElements();
-	}
-
-	createElements() {
+     createElements() {
 		for (let index = 0; index < this.cellsAmount; index++) {
-			// Cases fixes du puzzle
-			const puzzleDiv = document.createElement('div');
-			puzzleDiv.setAttribute('data-index', index);
-			this.puzzle.append(puzzleDiv);
-			this.puzzleDivs.push(puzzleDiv);
+			const puzzleDiv = document.createElement('div')
+			puzzleDiv.setAttribute('data-index', index)
+			this.puzzle.append(puzzleDiv)
+			this.puzzleDivs.push(puzzleDiv) 
 
-			// Pièces à déplacer
-			const draggableDiv = document.createElement('div');
-			draggableDiv.setAttribute('data-index', index);
-			draggableDiv.setAttribute('draggable', true);
-			this.draggableDivs.push(draggableDiv);
-		}
-	}
-}
+            const draggableDivs = document.createElement('div')
+			draggableDivs.setAttribute('data-index', index)
+			draggableDivs.setAttribute('draggable', true)
+			this.draggableDivs.push(draggableDivs) 
+         } 
+      } 
+    }
 
-export default Elements;
+
+export default Elements
