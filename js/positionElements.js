4ea830878db25623820f3085d53cc2350dcc91a5
alaye771 <65874110+alaye771@users.noreply.github.com>
2025-08-20 21:47:54 +0200

    Update positionElements.js

--- js/positionElements.js
+++ js/positionElements.js
@@ -5,8 +5,8 @@ class PositionElements {
 		this.elements = new Elements() 
 		this.leftPositions = [0, 8, 16, 24, 32]
 		this.topPositions = [0, 6, 12, 18]
-		this.addDraggableDivs()
-		
+        this.addDraggableDivs()
+	
     } 
 
 	shuffle(array) {
